2d02ae15b79340729a2195d88ac757f0861ae522
jafow <jared.a.fowler@gmail.com>
2016-05-25 16:20:38 -0700

    move node class into lib/

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-const crypto = require('crypto')
 const node = require('./lib/node.js')
-
-// Each Node has a random 160bit ID
-function Node () {
-  this.nodeID = ''
-  this.idLength = 20
-}
-
-Node.prototype.generateRandomID = function () {
-  // or use the crypto module
-  let id = crypto.randomBytes(this.idLength)
-  let idString = new Buffer(id)
-  this.nodeID = idString.toString('hex')
-  return this.nodeID
-}
-
 module.exports = node
